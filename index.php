3fc17a167cae71d57ce6fe1377d1922c747a20ec
Avartos <59014772+Avartos@users.noreply.github.com>
2020-12-06 13:51:17 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,141 @@
+<?php $numberOfSlides = 4 ?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>MaskYourFace</title>
+    <link rel="stylesheet" href="./Static/css/Slideshow.css" />
+</head>
+<body>
+<header>
+    <?include ("./Shared/Header.html");?>
+</header>
+<section>
+   <div>
+       <?include ("./Shared/Navbar.html");?>
+   </div>
+
+
+<div>
+    <h2>Unsere Dauerbrenner - Die halten alles auf!</h2>
+</div>
+        <div>
+            <!-- Product Spotlight -->
+            <?php include './Shared/Product_Spotlight.php' ?>
+        </div>>
+
+</section>
+<section>
+    <div>
+        <div>
+            <h2>Masken, Masken, Masken</h2>
+            <p>
+                Unsere Masken sind ein bewertes Produkt, was Milliarden Kindern Arbeit liefert. <br>
+                Nur Kinderhände sind in der Lage so feine Lagen zu legen und solch zierliche Stiche zu setzen
+            </p>
+            <br>
+        </div>
+        <div class="card">
+            <div class="container">
+                <img src="Static/assets/mask01.png" width="400">
+                <a href="">Maske 1</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="container">
+                <img src="Static/assets/mask01.png" width="400">
+                <a href="">Maske 2</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="container">
+                <img src="Static/assets/mask01.png" width="400">
+                <a href="">Maske 3</a>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="container">
+            <img src="Static/assets/mask01.png" width="400">
+            M<a href="">Maske 4</a>
+        </div>
+    </div>
+    <div class="card">
+        <div class="container">
+            <img src="Static/assets/mask01.png" width="400">
+            <a href="">Maske 5</a>
+        </div>
+    </div>
+    <div class="card">
+        <div class="container">
+            <img src="Static/assets/mask01.png" width="400">
+            <a href="">Maske 6</a>
+        </div>
+    </div>
+    </div>
+
+
+
+</section>
+<br>
+<section>
+
+    <div>
+        <h2>Schöner können Sie ihr Gesicht nicht verstecken. Die Götter wissen, dass sie es brauchen</h2>
+    </div>
+    <p>
+        Unsere geklauten Motive werden ihr Leben bereichern <br>
+        Mit unseren Masken wird es Ihnen so unendlich leicht gemacht Ihre menschenverachtenden Gesichtszüge zu verdecken.
+        Nutzen Sie diese Möglichkeit!!!!
+    </p>
+    <br>
+    </div>
+    <div class="card">
+        <div class="container">
+            <img src="Static/assets/mask01.png" width="400">
+            <a href="">Maske 7</a>
+        </div>
+    </div>
+    <div class="card">
+        <div class="container">
+            <img src="Static/assets/mask01.png" width="400">
+            <a href="">Maske 8</a>
+        </div>
+    </div>
+    <div class="card">
+        <div class="container">
+            <img src="Static/assets/mask01.png" width="400">
+            <a href="">Maske 9</a>
+        </div>
+    </div>
+    </div>
+    <div class="card">
+        <div class="container">
+            <img src="Static/assets/mask01.png" width="400">
+            M<a href="">Maske 10</a>
+        </div>
+    </div>
+    <div class="card">
+        <div class="container">
+            <img src="Static/assets/mask01.png" width="400">
+            <a href="">Maske 12</a>
+        </div>
+    </div>
+    <div class="card">
+        <div class="container">
+            <img src="Static/assets/mask01.png" width="400">
+            <a href="">Maske 13</a>
+        </div>
+
+</section>
+
+<footer>
+    <div class="footer">
+    <?include ("./Shared/Footer.html");?>
+    </div>
+</footer>
+
+<? include ("./Shared/up_button.html") ?>
+</body>
+</html>
