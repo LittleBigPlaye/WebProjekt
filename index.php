8cbf21e0138878175c758abbd3ce0d60a04ea3c0
Robin <robin.beck.we@gmail.com>
2020-12-11 17:21:41 +0100

    Added new checkbox-based navigation

--- index.php
+++ index.php
@@ -69,12 +69,13 @@ else
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="de">
 
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="stylesheet" href="assets/styles/navbar.css">
     <title>Mask Your Face</title>
 </head>
 
