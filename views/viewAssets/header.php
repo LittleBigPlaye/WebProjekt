70c7aacf8e019f93dc8749bef460878c23a35dc0
Avartos <59014772+Avartos@users.noreply.github.com>
2020-12-14 11:35:10 +0100

    logo geändert und angepasst

--- views/viewAssets/header.php
+++ views/viewAssets/header.php
@@ -1,6 +1,6 @@
 <div class="header">
     <div class="logo">
     <br>
-    <img src="./assets/images/logo.png" width="280">
+    <img src="./assets/images/logo2.png" width="580">
     </div>
 </div>
