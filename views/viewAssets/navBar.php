26c1908867fad915e5ac577ce837ff9b00b71c28
LittleBigPlaye <robin.beck.we@gmail.com>
2020-12-27 12:25:43 +0100

    changed click behaviour of navbar in mobile mode

--- views/viewAssets/navBar.php
+++ views/viewAssets/navBar.php
@@ -8,10 +8,11 @@
         <li <?= ($currentPosition == 'index') ? 'class="active"' : '' ?>><a href="?c=pages&a=index" >Startseite</a></li>
 
         <!-- Products -->
-        <li <?= ($currentPosition == 'products') ? 'class="active"' : '' ?>><a>Produkte
+        <li <?= ($currentPosition == 'products') ? 'class="active"' : '' ?>><label for="dropToggle01"><a>Produkte
                 <span class="dropIcon">▾</span>
                 <label title="toggle dropDown" class="dropIcon" for="dropToggle01">▾</label>
             </a>
+            </label>
             <input type="checkbox" id="dropToggle01">
             <ul class="subNav">
                 <li><a href="?c=products&a=listProducts">Alle Artikel</a></li>
@@ -25,10 +26,11 @@
         <li <?= ($currentPosition == 'login') ? 'class="right active"': 'class="right"'?>><a href="?c=pages&a=login">Login</a></li>
         
         <!-- Administration -->
-        <li class="right"><a>Administration
+        <li class="right"><label for="dropToggle02"><a>Administration
                 <span class="dropIcon">▾</span>
                 <label title="toggle dropDown" class="dropIcon" for="dropToggle02">▾</label>
             </a>
+            </label>
             <input type="checkbox" id="dropToggle02">
             <ul class="subNav">
                 <li><a href="#">Mein Konto</a></li>
