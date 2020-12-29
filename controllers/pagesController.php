9940680d1d6fca88da725b419d1acc1f47019ba8
LittleBigPlaye <robin.beck.we@gmail.com>
2020-12-29 11:47:40 +0100

    added todos

--- controllers/pagesController.php
+++ controllers/pagesController.php
@@ -20,12 +20,44 @@
 
      public function actionLogin()
      {
-         $this->setParam('currentPosition', 'login');
+        $this->setParam('currentPosition', 'login');
+        //TODO: store error message
+        $errorMessage = null;
+
+        //TODO: check if form is submitted
+        
+            //TODO: retrieve input
+
+            //TODO: validate values
+
+                //TODO: check if user exists
+
+                //TODO: check if password hash is valid
+                
+                    //TODO: get current user from Database
+            
+                    //TODO: set values in current user
+
+                    //TODO: store useful information (user and is logged in in session)
+                    //$_SESSION['login'] = serialize($currentLogin); 
+                    //TODO: redirect to previous page
+                    //header('Location: index.php?c=pages&a=index');
+
+                //TODO: increase failed login count, if password is wrong
+
+            //TODO: set error Message, if values are empty
+
+            //TODO: set param to prefill input fields
+            //$this->setParam('ParamName', $parameter);
      }
+
      public function actionRegister()
      {
          $this->setParam('currentPosition', 'register');
      }
+
+     //TODO: logout action
+
 
      public function actionShoppingCart()
      {
