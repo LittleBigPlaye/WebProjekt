def4a81369a88edf42d26dbc1bfd51890e8aaedb
Avartos <59014772+Avartos@users.noreply.github.com>
2021-01-08 10:20:57 +0100

    Registrierung funktioniert

--- views/accounts/register.php
+++ views/accounts/register.php
@@ -22,26 +22,26 @@
                   <h2>Persönliche Daten</h2>
                   <br>
                   <label class="registerlabel" for="name">Vorname:</label>
-                  <input class="input_register1" type="text" id="firstName" name="firstName">
+                  <input class="input_register1" type="text" id="firstName" name="firstName" placeholder="Vorname" value="<?=htmlspecialchars($_POST['firstName'] ?? '')?>">
                   <br>
 
                     <label class="registerlabel" for="name">Zweitname:</label>
-                    <input class="input_register1" type="text" id="secondName" name="secondName">
+                    <input class="input_register1" type="text" id="secondName" name="secondName" placeholder="zweiter Vorname" value="<?=htmlspecialchars($_POST['secondName'] ?? '')?>">
                   <br>
 
                   <label class="registerlabel" for="name">Nachname:</label>
-                  <input class="input_register1" type="text" id="lastName" name="lastName">
+                  <input class="input_register1" type="text" id="lastName" name="lastName" placeholder="Nachname" value="<?=htmlspecialchars($_POST['lastName'] ?? '')?>">
                   <br>
 
                   <label class="registerlabel" for="mail">Email:</label>
-                  <input class="input_register1" type="email" id="email" name="email">
+                  <input class="input_register1" type="email" id="email" name="email" placeholder="E-Mail" value="<?=htmlspecialchars($_POST['email'] ?? '')?>">
                   <br>
 
                   <label class="registerlabel" for="password">Passwort:</label>
-                  <input class="input_register1" type="password" id="password" name="password">
+                  <input class="input_register1" type="password" id="password" name="password" placeholder="Passwort">
                   <br>
                   <label class="registerlabel" for="password2">Passwort wiederholen:</label>
-                  <input class="input_register1" type="password" id="password2" name="password2">
+                  <input class="input_register1" type="password" id="password2" name="password2" placeholder="Passwort wiederholen">
                   <br>
 
                   <label>Geschlecht:</label>
@@ -50,7 +50,7 @@
                   <input type="radio" id="divers" value="u" name="gender"><label for="divers" class="light">Divers</label><br><br>
                   <br>
                   <label>Geburtstag:</label>
-                  <input class="input_register1" type="date" id="birthdate" name="birthdate">
+                  <input class="input_register1" type="date" id="birthdate" name="birthdate" placeholder="Geburtstag" value="<?=htmlspecialchars($_POST['birthdate'] ?? '')?>">
                   <br>
 
 
@@ -63,16 +63,16 @@
                   <br>
 
                   <label class="registerlabel">Strasse</label>
-                  <input class="input_register1" type="text" id="street" name="street">
+                  <input class="input_register1" type="text" id="street" name="street" placeholder="Strasse" value="<?=htmlspecialchars($_POST['street'] ?? '')?>">
                   <br>
                   <label class="registerlabel">Hausnummer</label>
-                  <input class="input_register1" type="text" id="streetNumber" name="streetNumber">
+                  <input class="input_register1" type="text" id="streetNumber" name="streetNumber" placeholder="Hausnummer" value="<?=htmlspecialchars($_POST['streetNumber'] ?? '')?>">
                   <br>
                   <label class="registerlabel" >Postleitzahl</label>
-                  <input class="input_register1" type="text" id="zipCode" name="zipCode">
+                  <input class="input_register1" type="text" id="zipCode" name="zipCode" placeholder="Postleitzahl" value="<?=htmlspecialchars($_POST['zipCode'] ?? '')?>">
                   <br>
                   <label class="registerlabel">Stadt</label>
-                  <input class="input_register1" type="text" id="city" name="city">
+                  <input class="input_register1" type="text" id="city" name="city" placeholder="Stadt" value="<?=htmlspecialchars($_POST['city'] ?? '')?>">
                   <br>
 
 
