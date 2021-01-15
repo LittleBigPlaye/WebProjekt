b4694d98c47f473de11b845d2bca8a38427570bd
Avartos <59014772+Avartos@users.noreply.github.com>
2021-01-15 09:10:57 +0100

    kleine Änderungen Benutzerverwaltung funktioniert wieder, sieht aber kacke aus

--- views/accounts/adminusermanagement.php
+++ views/accounts/adminusermanagement.php
@@ -1,5 +1,5 @@
 <div class="formWrapper">
-    <form method="post" action="?c=accounts&a=adminusermanagement">
+    <div>
         <div class="confirmTable">
             <div class="tableRow head">
                 <div class="tableCell">LoginID</div>
@@ -13,51 +13,53 @@
             </div>
 
             <?php foreach($logins as $login) : ?>
-            <div class="tableRow">
-                <input id="user" name="user" class="tableCell" value="<?=htmlspecialchars($login->user->id)?>" </input>
+                <form method="post" action="?c=accounts&a=adminusermanagement">
+                <div class="tableRow">
+                    <input id="user" name="user" class="tableCell" value="<?=htmlspecialchars($login->user->id)?>" </input>
 
-                <div class="tableCell"><?=htmlspecialchars($login->user->firstName)?></div>
-                <div class="tableCell"><?=htmlspecialchars($login->user->lastName)?></div>
-                <div class="tableCell">
-                    <select name="role">
-                        <option value="admin"
-                            <?= ($login->user->role === 'admin') ? 'selected' : '' ?>
-                        >Admin</option>
-                        <option value="user"
-                            <?= ($login->user->role === 'user') ? 'selected' : '' ?>
-                        >Nutzer</option>
-                    </select>
+                    <div class="tableCell"><?=htmlspecialchars($login->user->firstName)?></div>
+                    <div class="tableCell"><?=htmlspecialchars($login->user->lastName)?></div>
+                    <div class="tableCell">
+                        <select name="role">
+                            <option value="admin"
+                                <?= ($login->user->role === 'admin') ? 'selected' : '' ?>
+                            >Admin</option>
+                            <option value="user"
+                                <?= ($login->user->role === 'user') ? 'selected' : '' ?>
+                            >Nutzer</option>
+                        </select>
+                    </div>
+                    <div class="tableCell">
+                        <select name="validated">
+                            <option value="1"
+                                <?= ($login->validated) ? 'selected' : '' ?>
+                            >validated</option>
+                            <option value="0"
+                                <?= ($login->validated) ? '' : 'selected' ?>
+                            >unvalidated</option>
+                        </select>
+                    </div>
+                    <div class="tableCell">
+                        <select name="enabled">
+                            <option value="1"
+                                <?= ($login->enabled) ? 'selected' : '' ?>
+                            >enabled</option>
+                            <option value="0"
+                                <?= ($login->enabled) ? '' : 'selected' ?>
+                            >disabled</option>
+                        </select>
+                    </div>
+                    <div class="tableCell">
+                        <div> <input type="checkbox" id="passwordReset" name="passwordReset" > </div>
+                    </div>
+                    <div class="tableCell">
+                        <button class="buttonForAll" type="submit" name="saveChanges">Änderung Speichern</button>
+                    </div>
                 </div>
-                <div class="tableCell">
-                    <select name="validated">
-                        <option value="1"
-                            <?= ($login->validated) ? 'selected' : '' ?>
-                        >validated</option>
-                        <option value="0"
-                            <?= ($login->validated) ? '' : 'selected' ?>
-                        >unvalidated</option>
-                    </select>
-                </div>
-                <div class="tableCell">
-                    <select name="enabled">
-                        <option value="1"
-                            <?= ($login->enabled) ? 'selected' : '' ?>
-                        >enabled</option>
-                        <option value="0"
-                            <?= ($login->enabled) ? '' : 'selected' ?>
-                        >disabled</option>
-                    </select>
-                </div>
-                <div class="tableCell">
-                    <div> <input type="checkbox" id="passwordReset" name="passwordReset" > </div>
-                </div>
-                <div class="tableCell">
-                    <button class="buttonForAll" type="submit" name="saveChanges">Änderung Speichern</button>
-                </div>
-            </div>
-            <!--<input type="hidden" id="user" name="user" value="<?/*= $login->user->id */?>">-->
+                <!--<input type="hidden" id="user" name="user" value="<?/*= $login->user->id */?>">-->
+                </form>
             <?php endforeach ?>
-    </form>
+        </div>
 </div>
 
 
