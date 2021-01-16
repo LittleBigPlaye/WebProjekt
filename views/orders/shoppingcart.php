ac86020a094388bcd2bedf671f1e2e58a06bccaa
LittleBigPlaye <robin.beck.we@gmail.com>
2021-01-16 22:20:30 +0100

    made some more css tweaks

--- views/orders/shoppingcart.php
+++ views/orders/shoppingcart.php
@@ -3,33 +3,42 @@
 </h1>
 <?php if($order !== null) : ?>
 <div class="cartListing">
-    <?php foreach ($order->orderItems as $orderItem):?>
-        <div class="cartEntry">
-            <img src="
-                    <?php
-                        if($orderItem->product->images != NULL)
-                        {
-                            echo htmlspecialchars($orderItem->product->images[0]->path);
-                        }
-                        else
-                        {
-                            echo FALLBACK_IMAGE;
-                        }
-                    ?>">
-            <br>
-            <form method="Post">
-                Bezeichnung: <?=$orderItem->product->productName?><br>
-                Artikelnummer: <?=$orderItem->product->id?><br>
-                Anzahl: <input type="number" min="0", step="1" name="quantity" value="<?=$orderItem->quantity?>"> Stück<br>
-                Preis: <?=$orderItem->actualPrice?> €<br>
-                <input type="hidden" name="pid" value="<?=$orderItem->product->id?>">
-                <button type="submit" name="updateCart">Änderungen übernehmen</button>
-            </form>
+    <div class="table"> 
+            <div class="tableRow head">
+                <div class="tableCell"></div>
+                <div class="tableCell"></div>
+                <div class="tableCell"></div>
+                <div class="tableCell"></div>
+                <div class="tableCell">Anzahl</div>
+            </div>
 
-        </div>
-    <?php endforeach; ?>
+            <?php foreach ($order->orderItems as $orderItem):?>
+                <div class="tableRow">
+                    <div class="tableCell">    
+                        <img class="productPreview" src="
+                            <?php
+                                if($orderItem->product->images != NULL)
+                                {
+                                    echo htmlspecialchars($orderItem->product->images[0]->path);
+                                }
+                                else
+                                {
+                                    echo FALLBACK_IMAGE;
+                                }
+                            ?>">
+                    </div>
+                    <div class="tableCell"> Bezeichnung: <?=$orderItem->product->productName?></div>
+                    <div class="tableCell">Artikelnummer: <?=$orderItem->product->id?></div>
+                    <div class="tableCell">Preis: <?=$orderItem->actualPrice?> €</div>
+                    <form class="tableCell" method="Post">
+                        <input type="number" min="0", step="1" name="quantity" value="<?=$orderItem->quantity?>">
+                        <input type="hidden" name="pid" value="<?=$orderItem->product->id?>">
+                        <button type="submit" name="updateCart">Änderungen übernehmen</button>
+                    </form>
+                </div>
+        <?php endforeach; ?>
+    </div>
 </div>
-
 <div class="cartTotal">
     <b>Gesamtpreis:</b> <?= $totalPrice ?> €
     <form action="index.php?c=orders&a=confirmOrder" method="POST">
