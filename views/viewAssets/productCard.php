310f095136470c20523e297655f239a1e5e2f849
Robin <robin.beck.we@gmail.com>
2021-01-16 16:19:31 +0100

    made changes to product cards

--- views/viewAssets/productCard.php
+++ views/viewAssets/productCard.php
@@ -1,30 +1,32 @@
-<div class="card" id="prod<?=$product->id?>">
-    <div class="container">
-        <div class="productPreview">
-            <img <?= $product->isHidden ? 'class="hidden"' : '' ?>src="
-                <?php
-                    if($product->images != NULL)
-                    {
-                        echo htmlspecialchars($product->images[0]->path);
-                    }
-                    else
-                    {
-                        echo FALLBACK_IMAGE;
-                    }
-                ?>">
-                <?php if(!$product->isHidden) : ?>
-                    <form method="POST" action="#prod<?=$product->id?>">
-                        <button class="iconButton" type="submit" name="addToCart" value="<?=$product->id?>"><img src="assets\images\icons\shopping_cart.svg"/></button>
-                    </form>
-                <?php else : ?>
+<div class="productCard" id="prod<?=$product->id?>">
+    <p><?= htmlspecialchars($product->productName) ?></p>
+    <div class="productPreview">
+        <img class="productImage<?= $product->isHidden ? ' hidden' : '' ?>" src="
+            <?php
+                if($product->images != NULL)
+                {
+                    echo htmlspecialchars($product->images[0]->path);
+                }
+                else
+                {
+                    echo FALLBACK_IMAGE;
+                }
+            ?>">
+
+            <?php if(!$product->isHidden) : ?>
+                <!-- add to cart button -->
+                <form class="badge" method="POST" action="#prod<?=$product->id?>">
+                    <button class="iconButton" type="submit" name="addToCart" value="<?=$product->id?>"><img src="assets\images\icons\shopping_cart.svg"/></button>
+                </form>
+
+            <?php else : ?>
+                <!-- hidden notification icon -->
+                <div class="badge">
                     <div class="hiddenIcon"><img src="assets\images\icons\hidden_icon.svg" alt="Unsichtbar" title="Unsichtbar"></div>
-                <?php endif ?>
-        </div>
-        
-            <i><?= $product->isHidden ? '[unsichtbar]' : ''?></i><br>
-            <b><?= htmlspecialchars($product->productName) ?></b><br>
-            <i><?= htmlspecialchars($product->catchPhrase) ?><br></i>
-            <b><?= htmlspecialchars($product->standardPrice . ' €')?></b><br>
-            <a href="index.php?c=products&a=view&pid=<?= htmlspecialchars($product->id) ?>">Anzeigen</a>
+                </div>
+            <?php endif ?>
     </div>
+    
+    <p class="price"><?= htmlspecialchars($product->standardPrice . ' €')?></p>
+    <p class="catchPhrase"><?= htmlspecialchars($product->catchPhrase) ?></p>
 </div>
