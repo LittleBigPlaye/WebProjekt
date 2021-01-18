8f3d60f93d86ee346dc9f6ab1c285d06d535b81b
Avartos <59014772+Avartos@users.noreply.github.com>
2021-01-18 12:36:57 +0100

    Die Schlanghe hat nur ein Auge

--- assets/javascript/accounts/whileYouWait.js
+++ assets/javascript/accounts/whileYouWait.js
@@ -123,7 +123,7 @@ function updateLoop() {
 
     if (gameOver === false && timeSinceLastTick > TIMEBETWEENTICKS) {
 
-        if (berechner > TIMEFURITGONEBAD) {
+        if ( berechner > TIMEFURITGONEBAD){
 
             berechner -= TIMEFURITGONEBAD;
 
@@ -137,132 +137,138 @@ function updateLoop() {
             gc.fillRect(badFoodX * CELLSIZE, badFoodY * CELLSIZE, CELLSIZE, CELLSIZE);
 
 
-            timeSinceLastTick -= TIMEBETWEENTICKS;
+        }
 
 
-            gc.fillStyle = "#000000";
-            gc.fillRect(0, 0, canvas.width, canvas.height - 200);
+
+        timeSinceLastTick -= TIMEBETWEENTICKS;
 
 
-            gc.fillStyle = "#ffffff";
-            gc.fillRect(0, 400, canvas.width, 200);
+        gc.fillStyle = "#838383";
+        gc.fillRect(0, 0, canvas.width, canvas.height - 200);
 
 
-            gc.fillStyle = "#000000";
-            gc.font = "bold 20px Arial";
-            gc.fillText("Punkte:  " + counter, 10, 420, 150);
+        gc.fillStyle = "#e3e3e3";
+        gc.fillRect(0, 400, canvas.width, 200);
 
 
-            if (controlQueue.length > 0) {
-
-                switch (controlQueue[0]) {
-                    case(LEFT):
-                        if (dir === UP || dir === DOWN) {
-                            dir = LEFT;
-                        }
-                        break;
-                    case(RIGHT):
-                        if (dir === UP || dir === DOWN) {
-                            dir = RIGHT;
-                        }
-                        break;
-                    case(DOWN):
-                        if (dir === LEFT || dir === RIGHT) {
-                            dir = DOWN;
-                        }
-                        break;
-                    case(UP):
-                        if (dir === LEFT || dir === RIGHT) {
-                            dir = UP;
-                        }
-                        break;
-                }
-
-                controlQueue.splice(0, 1);
-            }
+        gc.fillStyle = "#333";
+        gc.font = "bold 20px Arial";
+        gc.fillText("Punkte:  " + counter, 10, 420, 150);
 
 
-            switch (dir) {
+
+        if (controlQueue.length>0){
+
+            switch(controlQueue[0]) {
                 case(LEFT):
-                    x--;
+                    if(dir === UP || dir === DOWN) {
+                        dir = LEFT;
+                    }
                     break;
-
                 case(RIGHT):
-                    x++;
+                    if(dir === UP || dir === DOWN) {
+                        dir = RIGHT;
+                    }
                     break;
-
-                case(UP):
-                    y--;
-                    break;
-
                 case(DOWN):
-                    y++;
+                    if(dir === LEFT || dir === RIGHT) {
+                        dir = DOWN;
+                    }
+                    break;
+                case(UP):
+                    if(dir === LEFT || dir === RIGHT) {
+                        dir = UP;
+                    }
                     break;
             }
 
-            if (x < 0) {
-                x = GRIDWIDTH - 1;
+            controlQueue.splice(0, 1);
+        }
+
+
+
+        switch(dir) {
+            case(LEFT):
+                x--;
+                break;
+
+            case(RIGHT):
+                x++;
+                break;
+
+            case(UP):
+                y--;
+                break;
+
+            case(DOWN):
+                y++;
+                break;
+        }
+
+        if(x < 0) {
+            x = GRIDWIDTH -1;
+        }
+
+        if(x >= GRIDWIDTH) {
+            x = 0;
+        }
+
+        if(y < 0) {
+            y = GRIDHEIGHT -1;
+        }
+
+        if(y >= GRIDHEIGHT) {
+            y = 0;
+        }
+
+
+        for ( i = 0; i < segments.length; i++) {
+            if(segments[i].x === x && segments[i].y === y){
+                gameOver = true;
+                GAMEOVER();
             }
+        }
 
-            if (x >= GRIDWIDTH) {
-                x = 0;
-            }
-
-            if (y < 0) {
-                y = GRIDHEIGHT - 1;
-            }
-
-            if (y >= GRIDHEIGHT) {
-                y = 0;
-            }
+        for (var i = segments.length - 1; i > 0; i--) {
+            segments[i].x = segments[i-1].x;
+            segments[i].y = segments[i-1].y;
+        }
 
 
-            for (i = 0; i < segments.length; i++) {
-                if (segments[i].x === x && segments[i].y === y) {
-                    gameOver = true;
-                    GAMEOVER();
-                }
-            }
+        segments[0].x = x;
+        segments[0].y = y;
 
-            for (var i = segments.length - 1; i > 0; i--) {
-                segments[i].x = segments[i - 1].x;
-                segments[i].y = segments[i - 1].y;
-            }
-
-
-            segments[0].x = x;
-            segments[0].y = y;
-
-            if (x === foodX && y === foodY) {
-                new Segment(-100, -100);
-                counter++;
-                speedCounter++;
-                placeFood();
-
-            }
-
-
-            if (speedCounter === 10) {
-                speedCounter = 0;
-                TIMEBETWEENTICKS -= 0.01;
-
-            }
-
-            gc.fillStyle = "#ff6600";
-
-            for (var i = 0; i < segments.length; i++) {
-
-                gc.fillRect(segments[i].x * CELLSIZE, segments[i].y * CELLSIZE, CELLSIZE, CELLSIZE);
-
-            }
-
-
-            gc.fillStyle = "#0000ff";
-            gc.fillRect(foodX * CELLSIZE, foodY * CELLSIZE, CELLSIZE, CELLSIZE);
+        if(x === foodX && y === foodY){
+            new Segment (-100, -100);
+            counter ++;
+            speedCounter ++;
+            placeFood();
 
         }
+
+
+
+        if(speedCounter === 10){
+            speedCounter = 0;
+            TIMEBETWEENTICKS -= 0.01;
+
+        }
+
+        gc.fillStyle = "#000000";
+
+        for(var i = 0; i < segments.length; i++){
+
+            gc.fillRect(segments[i].x * CELLSIZE, segments[i].y * CELLSIZE, CELLSIZE, CELLSIZE);
+
+        }
+
+
+        gc.fillStyle = "#eb7d00";
+        gc.fillRect(foodX * CELLSIZE, foodY * CELLSIZE, CELLSIZE, CELLSIZE);
+
     }
-
-
-    window.setInterval(updateLoop, 1);
 }
+
+
+window.setInterval(updateLoop, 1);
--- views/accounts/waitingArea.php
+++ views/accounts/waitingArea.php
@@ -1,5 +1,5 @@
 <div class="formWrapper">
     <h1>Wie viele Punkte schaffen Sie?</h1>
     <canvas id="snakeCan" width="400" height="600"></canvas>
-    <script src=/assets/javascript/accounts/whileYouWait.js></script>
+    <script src="<?=JAVASCRIPTPATH . 'accounts' . DIRECTORY_SEPARATOR . 'whileYouWait.js'?>"></script>
 </div>
