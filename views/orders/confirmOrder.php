de5521f5474c125c056d0c63f59c7523d991074a
Robin <robin.beck.we@gmail.com>
2021-01-18 19:49:01 +0100

    added mobile view for pseudotable

--- views/orders/confirmOrder.php
+++ views/orders/confirmOrder.php
@@ -4,13 +4,13 @@
 
         <p>Bitte prüfen Sie vor dem Absenden der Bestellung, ob die nachfolgenden Angaben korrekt sind!</p>
 
-        <h2>Empfänger</h2>
+        <h2>Zieladresse</h2>
         <?= htmlspecialchars($user->salutation . ' ' . $user->firstName . ' ' . $user->secondName . ' ' . $user->lastName)?><br>
         <?= htmlspecialchars($address->street . ' ' . $address->streetNumber)?><br>
         <?= htmlspecialchars($address->zipCode . ' ' . $address->city)?><br>
         <!-- Hier Adresse anzeigen -->
 
-        <h2>Positionen</h2>
+        <h2>Kauf-Übersicht</h2>
         <div class="table orderConfirmation">
             <div class="tableRow head">
                 <div class="tableCell">Bezeichnung</div>
@@ -22,14 +22,26 @@
 
             <?php foreach ($order->orderItems as $orderItem):?>
                 <div class="tableRow">
-                    <div class="tableCell"><?=htmlspecialchars($orderItem->product->productName)?></div>
-                    <div class="tableCell right"><?=htmlspecialchars(str_pad($orderItem->product->id, 12, '0', STR_PAD_LEFT))?></div>
-                    <div class="tableCell right"><?=htmlspecialchars($orderItem->quantity)?> Stück</div>
-                    <div class="tableCell right"><?=htmlspecialchars($orderItem->actualPrice)?> €</div>
+                    <div class="tableCell">
+                        <label class="mobileLabel">Produktbezeichnung</label>
+                        <?=htmlspecialchars($orderItem->product->productName)?>
+                    </div>
+                    <div class="tableCell right">
+                        <label class="mobileLabel">Produkt-Nr.</label>
+                        <?=htmlspecialchars(str_pad($orderItem->product->id, 12, '0', STR_PAD_LEFT))?>
+                    </div>
+                    <div class="tableCell right">
+                        <label class="mobileLabel">Menge</label>
+                        <?=htmlspecialchars($orderItem->quantity)?> Stück
+                    </div>
+                    <div class="tableCell right">
+                        <label class="mobileLabel">Preis</label>
+                        <?=htmlspecialchars($orderItem->actualPrice)?> €
+                    </div>
                 </div>
             <?php endforeach; ?>
         
-            <div class="tableRow head">
+            <div class="tableRow bottom">
                     <div class="tableCell"></div>
                     <div class="tableCell"></div>
                     <div class="tableCell right">Gesamtpreis</div>
--- views/orders/shoppingcart.php
+++ views/orders/shoppingcart.php
@@ -1,55 +1,52 @@
-<?php
-    $cartCount = isset($_SESSION['shoppingCart']) ? array_sum($_SESSION['shoppingCart']) : '';
-?>
-<h1>
-    Ihr Warenkorb
-</h1>
+<?php $cartCount = isset($_SESSION['shoppingCart']) ? array_sum($_SESSION['shoppingCart']) : '';?>
+<h1>Warenkorb</h1>
+
 <?php if($order !== null) : ?>
     <div class="cartListing">
         <div class="table shoppingCart"> 
                 <div class="tableRow head">
+                    <div class="tableCell">Produkte</div>
                     <div class="tableCell"></div>
                     <div class="tableCell"></div>
-                    <div class="tableCell">Preis</div>
-                    <div class="tableCell">Anzahl</div>
+                    <div class="tableCell"></div>
                 </div>
 
-                <?php foreach ($order->orderItems as $index => $orderItem):?>
-                    <div class="tableRow">
-                        <div class="tableCell">    
-                            <a href="index.php?c=products&a=view&pid=<?=$orderItem->product->id?>">
-                                <img class="productPreview" src="
-                                    <?php
-                                        if($orderItem->product->images != NULL)
-                                        {
-                                            echo htmlspecialchars($orderItem->product->images[0]->thumbnailPath);
-                                        }
-                                        else
-                                        {
-                                            echo FALLBACK_IMAGE;
-                                        }
-                                    ?>">
-                            </a>
-                        </div>
-                        <div class="tableCell">
-                            <label for="productName<?=$index?>">Bezeichnung</label>
-                            <span id="productName<?=$index?>"><?=htmlspecialchars($orderItem->product->productName)?></span><br>
-                            <label for="productId<?=$index?>">Produkt-Nr.</label>
-                            <span id="productId<?=$index?>"><?=htmlspecialchars(str_pad($orderItem->product->id, 12, '0', STR_PAD_LEFT))?></span>
-                        </div>
-
-                        <div class="tableCell">
-                            <label for="price<?=$index?>">Preis</label>
-                            <span id="price<?=$index?>"><?=htmlspecialchars($orderItem->actualPrice)?> €</span>
-                        </div>
-                        
-                        <form class="tableCell" method="Post">
-                            <label for="quantity<?=$index?>">Menge</label>
-                            <input id="quantity<?=$index?>" class="quantitySelector" type="number" min="0", step="1" name="quantity" value="<?=$orderItem->quantity?>">
-                            <button class="cartQuantitySubmit" type="submit" name="updateCart">Änderungen übernehmen</button>
-                            <input type="hidden" name="pid" value="<?=$orderItem->product->id?>">
-                        </form>
+            <?php foreach ($order->orderItems as $index => $orderItem):?>
+                <div class="tableRow">
+                    <div class="tableCell">    
+                        <a href="index.php?c=products&a=view&pid=<?=$orderItem->product->id?>">
+                            <img class="productPreview" src="
+                                <?php
+                                    if($orderItem->product->images != NULL)
+                                    {
+                                        echo htmlspecialchars($orderItem->product->images[0]->thumbnailPath);
+                                    }
+                                    else
+                                    {
+                                        echo FALLBACK_IMAGE;
+                                    }
+                                ?>">
+                        </a>
                     </div>
+                    <div class="tableCell">
+                        <label for="productName<?=$index?>">Bezeichnung</label>
+                        <span id="productName<?=$index?>"><?=htmlspecialchars($orderItem->product->productName)?></span><br>
+                        <label for="productId<?=$index?>">Produkt-Nr.</label>
+                        <span id="productId<?=$index?>"><?=htmlspecialchars(str_pad($orderItem->product->id, 12, '0', STR_PAD_LEFT))?></span>
+                    </div>
+
+                    <div class="tableCell">
+                        <label for="price<?=$index?>">Preis</label>
+                        <span id="price<?=$index?>"><?=htmlspecialchars($orderItem->actualPrice)?> €</span>
+                    </div>
+                    
+                    <form class="tableCell" method="Post">
+                        <label for="quantity<?=$index?>">Menge</label>
+                        <input id="quantity<?=$index?>" class="quantitySelector" type="number" min="0", step="1" name="quantity" value="<?=$orderItem->quantity?>">
+                        <button class="cartQuantitySubmit" type="submit" name="updateCart">Änderungen übernehmen</button>
+                        <input type="hidden" name="pid" value="<?=$orderItem->product->id?>">
+                    </form>
+                </div>
             <?php endforeach; ?>
         </div>
     </div>
