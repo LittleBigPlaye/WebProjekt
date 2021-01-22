cbd6de39d8de2ee86c2e94419924e9be4c059540
Robin <robin.beck.we@gmail.com>
2021-01-22 10:52:46 +0100

    renamed file to fix small error

--- assets/javascript/accounts/validatePersonas.js
+++ assets/javascript/accounts/validatePersonas.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var btnSubmit = document.getElementById('submit');
 
-//get input fields from form
+    //get input fields from form
     var firstName = document.getElementById('firstName');
     var lastName = document.getElementById('lastName');
     var birthDate = document.getElementById('birthDate');
@@ -9,38 +9,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
     //check if submit button has been found
     if (btnSubmit) {
-        btnSubmit.addEventListener('click', function(event)
-        {
+        btnSubmit.addEventListener('click', function(event) {
             var formIsValid = true;
 
             //check if firstName is set
-            if (!firstName || firstName.value.length <= 0)
-            {
+            if (!firstName || firstName.value.length <= 0) {
                 firstName.classList.add("errorHighlight");
                 formIsValid = false;
-            }
-            else
-            {
+            } else {
                 firstName.classList.remove('errorHighlight');
             }
 
             //check if lastName is set
-            if (!lastName || lastName.value.length <= 0)
-            {
+            if (!lastName || lastName.value.length <= 0) {
                 lastName.classList.add("errorHighlight");
                 formIsValid = false;
-            } else
-            {
+            } else {
                 lastName.classList.remove('errorHighlight');
             }
 
             //check if birthDate is set
-            if (!birthDate || birthDate.value.length <= 0)
-            {
+            if (!birthDate || birthDate.value.length <= 0) {
                 birthDate.classList.add("errorHighlight");
                 formIsValid = false;
-            } else
-            {
+            } else {
                 birthDate.classList.remove('errorHighlight');
             }
 
