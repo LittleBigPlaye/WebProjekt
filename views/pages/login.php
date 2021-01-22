86d7055a2c72599b9aa43f070c5cf7c2566933ee
Hannes <lenz-md1@web.de>
2021-01-22 16:03:18 +0100

    js login check

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,20 +1,21 @@
-<?php if(!empty($errorMessage)) : ?>
-    <div class="errorMessage">
-        <span class="messageClose" onclick="this.parentElement.style.display='none';">&times</span>
-        <?= $errorMessage ?>
-    </div>
-<?php endif ?>
 <div class="formWrapper">
-  <form class="loginForm" action="index.php?c=pages&a=login" method="post">
-    <h1>Login</h1>
-    <label for="mail">Email:</label>
-    <input type="email" id="mail" name="user_email" value="<?= htmlspecialchars($_POST['user_email'] ?? '') ?>">
+    <form class="loginForm" action="index.php?c=pages&a=login" method="post">
+        <h1>Login</h1>
+        <?php foreach($errorMessages as $message) : ?>
+            <div class="errorMessage">
+                <span class="messageClose" onclick="this.parentElement.style.display='none';">&times</span>
+                <?= $message ?>
+            </div>
+        <?php endforeach; ?>
+        <div class="input"><label for="email">Email:</label>
+            <input type="text" id="email" name="email" value="<?= htmlspecialchars($_POST['email'] ?? '') ?>">
+            <span class="errorInfo">Bitte geben Sie eine korrekte E-Mail an!</span>
+        </div>
+        <label for="password">Passwort:</label>
+        <input type="password" id="password" name="user_password">
 
-    <label for="password">Passwort:</label>
-    <input type="password" id="password" name="user_password">
-
-    <input type="submit" name="submit" value="Anmelden"/>
-    <p>Sie haben noch keinen Account? <br><a href="index.php?c=accounts&a=register">Hier registrieren!</a></p>
-  </form>
+        <input type="submit" name="submit" id="submit" value="Anmelden"/>
+        <p>Sie haben noch keinen Account? <br><a href="index.php?c=accounts&a=register">Hier registrieren!</a></p>
+    </form>
 </div>
-
+<script src="<?=JAVASCRIPTPATH . 'pages' . DIRECTORY_SEPARATOR . 'validateLogin.js'?>"></script>
