7ba15e46cabeebee39de6ed58b31940161f1222c
Robin <robin.beck.we@gmail.com>
2021-01-28 14:59:58 +0100

    added stuff to the documentation

--- views/viewAssets/header.php
+++ views/viewAssets/header.php
@@ -1,12 +1,6 @@
 <header>
     <div class="header">
-        <div class="logo1">
-            <br>
-            <img src="./assets/images/logo2.png" width="580">
-        </div>
-        <div class="logo2">
-            <br>
-            <img src="./assets/images/logo.png" width="250">
-        </div>
+        <img class="logo1" src="./assets/images/logo2.png">
+        <img class="logo2" src="./assets/images/logo.png">
     </div>
 </header>
