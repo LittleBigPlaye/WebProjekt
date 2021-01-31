98c65e4515283112b86268315bb80d6eaae55e7d
Avartos <59014772+Avartos@users.noreply.github.com>
2021-01-31 01:54:47 +0100

    Footer überarbeitet

--- views/viewAssets/footer.php
+++ views/viewAssets/footer.php
@@ -1,26 +1,30 @@
 <footer>
     <div class="footer">
-        MaskYourFace
-        <br>
-        Vertrieb von Mund-Nasenschutzmasken
-        <br>
-        <a href="index.php?c=pages&a=imprint">Impressum</a>
-        <a href="documentation/documentation.html">Dokumentation</a>
-        <br>
+        <div>
+            <h2>MaskYourFace</h2>
+            Vertrieb von Mund-Nasenschutzmasken
+            <br>
+            <a href="index.php?c=pages&a=imprint">Impressum</a>
+            <br>
+            <a href="documentation/documentation.html">Dokumentation</a>
+            <br>
+        </div>
         <hr>
-        Inhaber:
-        <br>
-        Robin Beck
-        </br>
-        Hannes Lenz
-        <br>
-        John Klippstein
-        <br>
+        <div>
+            <h2>Inhaber:</h2>
+            Robin Beck
+            </br>
+            Hannes Lenz
+            <br>
+            John Klippstein
+            <br>
+        </div>
         <hr>
-        Anschrift:
-        <br>
-        Fantasieweg 1337
-        <br>
-        121212 Knechtschaftsburg
+        <div>
+            <h2>Anschrift:</h2>
+            Fantasieweg 1337
+            <br>
+            121212 Knechtschaftsburg
+        </div>
     </div>
 </footer>
