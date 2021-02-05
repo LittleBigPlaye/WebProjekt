38a0cc9840991ce1e3af7aa182fc090a00414994
Avartos <59014772+Avartos@users.noreply.github.com>
2021-02-05 10:06:19 +0100

    kleine Änderungen

--- views/accounts/register.php
+++ views/accounts/register.php
@@ -17,9 +17,9 @@
             </div>
         <?php endforeach; ?>
 
-        <?php if(isset($succesMessage) && !empty($succesMessage)) : ?>
+        <?php if(isset($successMessage) && !empty($successMessage)) : ?>
             <input class="messageToggle" type="checkbox" id="successToggle">
-            <div class="message successMessage">
+            <div class="message successMessages">
                 <label class="messageClose" for="successToggle">&times</label>
                 <p><?= $successMessage ?></p>
             </div>
