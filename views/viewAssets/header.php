ebee4d5b21602ffb4393cde31a410b20b987455b
LittleBigPlaye <robin.beck.we@gmail.com>
2021-02-09 17:58:38 +0100

    added link to header

--- views/viewAssets/header.php
+++ views/viewAssets/header.php
@@ -1,6 +1,8 @@
 <header>
     <div class="header">
-        <img class="logo1" src="./assets/images/logo2.png">
-        <img class="logo2" src="./assets/images/logo.png">
+        <a href="index.php">
+            <img class="logo1" src="./assets/images/logo2.png">
+            <img class="logo2" src="./assets/images/logo.png">
+        </a>
     </div>
 </header>
