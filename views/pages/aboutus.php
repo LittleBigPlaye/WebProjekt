6d6bc0ddb903ac31e037e876de6fc80e5a0a45ce
LittleBigPlaye <robin.beck.we@gmail.com>
2021-02-23 08:26:06 +0100

    fixed typos in about us

--- views/pages/aboutus.php
+++ views/pages/aboutus.php
@@ -2,45 +2,47 @@
     <h1>Über Uns/About Us/Unsere abgrund-tiefsten Geheimnisse</h1>
     <section>
         <h3>Die Menschen bei MaskYourFace</h3>
-            <p>
-                Wir wollen Menschen etwas Gutes tun und ihren Alltag mit unseren Produkten verschönern. Genau diese
-                Einstellung leben wir vor und erwarten diese von jedem, der sich unseren Team anschließen möchte. Humor
-                und Spaß bei der Arbeit sind uns ebenso wichtig, wie die herausrragende Qualitität unserer Produkte.
-                Wir hoffen das die Menschen unseren Spirit in jedem cm² unserer Arbeit wiederfinden können und somit ein
-                Produkt in den Händen halten das sie nicht nur schützt, sondern ihnen auch Spaß beim tragen bereitet.
-            </p>
+        <p>
+            Wir wollen Menschen etwas Gutes tun und ihren Alltag mit unseren Produkten verschönern. Genau diese
+            Einstellung leben wir vor und erwarten diese von jedem, der sich unseren Team anschließen möchte. Humor
+            und Spaß bei der Arbeit sind uns ebenso wichtig, wie die herausragende Qualitität unserer Produkte.
+            Wir hoffen das die Menschen unseren Spirit in jedem cm² unserer Arbeit wiederfinden können und somit ein
+            Produkt in den Händen halten das sie nicht nur schützt, sondern ihnen auch Spaß beim Tragen bereitet.
+        </p>
     </section>
 
     <section>
         <h3>Idee</h3>
-            <p>Wir von MaskYourFace sind schon immer darauf bedacht die für den Alltag nötigen Dinge mit Pepp zu
-                gestalten, da der Bedarf an Masken gerade unsagbar hoch ist und sie von vielen den ganzen Tag getragen werden muss,
-                haben wir uns das Ziel gesetzt qualitativ hochwertige Masken für den Alltag zu gestalten. Mit einem design
-                dass entweder zu dem Outfit passt oder die Leute unter ihren Masken zum lächeln bringt.
-            </p>
+        <p>
+            Wir von MaskYourFace sind schon immer darauf bedacht die für den Alltag nötigen Dinge mit Pepp zu
+            gestalten, da der Bedarf an Masken gerade unsagbar hoch ist und Masken von vielen den ganzen Tag getragen werden müssen,
+            haben wir uns das Ziel gesetzt qualitativ hochwertige Masken für den Alltag zu gestalten. Mit einem Design,
+            dass entweder zu dem Outfit passt oder die Leute unter ihren Masken zum Lächeln bringt.
+        </p>
     </section>
 
     <section>
         <h3>Produktion</h3>
-            <p> Da unser Produkt für den Alltag gedacht ist haben wir auch entsprechend hohe Anforderungen an die Qualität.
-                Das beinhaltet die Lebensdauer des Stoffes und der Farbe des Aufdrucks sodass man die Masken auch waschen kann ohne
-                Angst haben zu müssen dass die Farbe verblasst. Ebenso wie die Bequemlichkeit beim Tragen der Maske über
-                längere mehrere Stunden am Stück. Wir stellen unsere Produkte in Deutschland her und für die Bezahlung
-                der Arbeitnehmer exitiert ein Tarifvertrag der vom Betriebsrat gestützt wird.
-            </p>
+        <p> 
+            Da unser Produkt für den Alltag gedacht ist, haben wir auch entsprechend hohe Anforderungen an die Qualität.
+            Das beinhaltet die Lebensdauer des Stoffes und der Farbe des Aufdrucks sodass man die Masken auch waschen kann ohne
+            Angst haben zu müssen dass die Farbe verblasst. Ebenso wie die Bequemlichkeit beim Tragen der Maske über
+            mehrere Stunden am Stück. Wir stellen unsere Produkte in Deutschland her und für die Bezahlung
+            der Arbeitnehmer exitiert ein Tarifvertrag der vom Betriebsrat gestützt wird.
+        </p>
     </section>
 
     <section>
         <h3>Versprechen</h3>
-            <p>
-                Unsere Produkte sind langlebig und wir liefern zu jeder Maske eine Beschreibung mit, wie diese zu tragen
-                und zu pflegen ist. Wenn diese Hinweise beachtet und eingehaltet werden, haben Sie, als Kunde, ein Produkt
-                das sie viele Jahre begleiten und Sie schützen wird.
-            </p>
-            <p>
-                Für all unsere Produkte geben wir euch unsere Qualiätsgarantie, wenn sich innerhalb des ersten Jahres nach
-                dem Erhalt des Produkts ein Problem ergeben sollte, gibt es natürlich eine Entschädigung. Natürlich
-                sind Schäden durch Verschleiß und unsachgemäßen Gebrauch ausgeschlossen.
-            </p>
+        <p>
+            Unsere Produkte sind langlebig und wir liefern zu jeder Maske eine Beschreibung mit, wie diese zu tragen
+            und zu pflegen ist. Wenn diese Hinweise beachtet und eingehaltet werden, haben Sie, als Kunde, ein Produkt
+            das Sie viele Jahre begleiten und schützen wird.
+        </p>
+        <p>
+            Für all unsere Produkte geben wir Ihnen unsere Qualiätsgarantie. Wenn sich innerhalb des ersten Jahres nach
+            dem Erhalt des Produkts ein Problem ergeben sollte, gibt es natürlich eine Entschädigung. 
+            Schäden durch Verschleiß und unsachgemäßen Gebrauch sind ausgeschlossen.
+        </p>
     </section>
 </div>
